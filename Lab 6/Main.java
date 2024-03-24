0430af677ffef941d3c12ec9ebb59fd7aab2b982
Olena Sheremet <lenka.sheremet1978@gmail.com>
2024-03-24 17:03:42 +0200

    генерація паролів

---
+++ Lab 6/Main.java
@@ -0,0 +1,25 @@
+import java.security.SecureRandom;
+
+public class Main {
+    private static final String LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPERCASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String generatePassword(int length, boolean includeUppercase) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        String alphabet = LOWERCASE_ALPHABET;
+        if (includeUppercase) alphabet += UPPERCASE_ALPHABET;
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(alphabet.length());
+            password.append(alphabet.charAt(randomIndex));
+        }
+        return password.toString();
+    }
+    public static void main(String[] args) {
+        int length = 8; // Довжина паролю
+        boolean includeUppercase = true; // Включити великі літери
+        String password = generatePassword(length, includeUppercase);
+        System.out.println("Згенерований пароль: " + password);
+    }
+}
